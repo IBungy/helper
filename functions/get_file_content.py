c88a794fb29e168f60967b9dfaf82b63454d0a87
IBungy <bcain4902@gmail.com>
2025-08-13 15:41:42 -0400

    added get_file_content and updated tests.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    working_directory_abs = os.path.abspath(working_directory)
+    relative_directory_abs = os.path.abspath(os.path.join(working_directory, file_path))
+    if not relative_directory_abs.startswith(working_directory_abs):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(relative_directory_abs):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(relative_directory_abs, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            next_char = f.read(1)
+            if len(next_char) > 0:
+                file_content_string = file_content_string + f"[...File \"{file_path}\" truncated at {MAX_CHARS} characters]"
+        return file_content_string
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -3,14 +3,8 @@ from functions.get_files_info import get_files_info
 
 
 def tests():
-    first_test = get_files_info("calculator", ".")
-    print(f"Result for current directory:\n    {first_test}")
-    second_test = get_files_info("calculator", "pkg")
-    print(f"Result for 'pkg' directory:\n    {second_test}")
-    third_test = get_files_info("calculator", "/bin")
-    print(f"Result for '/bin' directory:\n    {third_test}")
-    fourth_test = get_files_info("calculator", "../")
-    print(f"Result for '../' directory:\n    {fourth_test}")
+    first_test = get_file_content("calculator", "lorem.txt")
+    print(first_test)
 
 if __name__ == "__main__":
     tests()
