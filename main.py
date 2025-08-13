23636143831649fc6f57a2f8dc62d0f51b239d78
IBungy <bcain4902@gmail.com>
2025-08-12 21:57:54 -0400

    added a chat history

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
 def main():
     if len(sys.argv) < 2:
@@ -13,7 +17,7 @@ def main():
         sys.exit(1)
     else:
         prompt = " ".join(sys.argv[1:])
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokens = response.usage_metadata.candidates_token_count
         print(response.text)
