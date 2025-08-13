1d4b2ef1e8e2e7e429032f2deaf99f9758c74ae3
IBungy <bcain4902@gmail.com>
2025-08-12 21:33:42 -0400

    fixed missing quotes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model=gemini-2.0-flash-001, contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
     print(response.text)
     print(f"Prompt token: {prompt_token_count.usage_metadata}")
     print(f"Response tokens: {candidates_token_count.usage_metadata}")
