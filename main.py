f2c58972a522a24717f49ac63d6a03cff52ce950
IBungy <bcain4902@gmail.com>
2025-08-12 22:00:26 -0400

    fixed bugs

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from google.genai import types
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
 
 def main():
     if len(sys.argv) < 2:
         print("Error: No prompt provided")
         sys.exit(1)
     else:
-        prompt = " ".join(sys.argv[1:])
+        user_prompt = " ".join(sys.argv[1:])
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
         response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokens = response.usage_metadata.candidates_token_count
