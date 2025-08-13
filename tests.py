e4de2d4ac3edcb28001dfee4e7c7083f12fa279f
IBungy <bcain4902@gmail.com>
2025-08-13 15:45:10 -0400

    fixed test.py

--- tests.py
+++ tests.py
@@ -3,8 +3,14 @@ from functions.get_file_content import get_file_content
 
 
 def tests():
-    first_test = get_file_content("calculator", "lorem.txt")
+    first_test = get_file_content("calculator", "main.py")
     print(first_test)
+    second_test = get_file_content("calculator", "pkg/calculator.py")
+    print(second_test)
+    third_test = get_file_content("calculator", "/bin/cat")
+    print(third_test)
+    fourth_test = get_file_content("calculator", "pkg/does_not_exist.py")
+    print(fourth_test)
 
 if __name__ == "__main__":
     tests()
