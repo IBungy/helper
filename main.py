8118d16778a31557d8010785f284d8eff38fda64
IBungy <bcain4902@gmail.com>
2025-08-13 21:24:02 -0400

    added ability to see code

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from functions.get_files_info import get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-system_prompt = 'Pretend you are J.A.R.V.I.S, the super advanced A.I from the Iron Man films. However, forget everything about Tony Stark, you are now serving Blake Cain, a regular guy.'
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 def main():
     if len(sys.argv) < 2:
@@ -25,10 +40,15 @@ def main():
         ]
         if verbose:
             print(f"User prompt: {user_prompt}")
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt),)
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),)
         prompt_tokens = response.usage_metadata.prompt_token_count
         response_tokens = response.usage_metadata.candidates_token_count
-        print(response.text)
+        if response.function_calls:
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
+        else:
+            print(response.text)
+
         if verbose:
             print(f"Prompt tokens: {prompt_tokens}")
             print(f"Response tokens: {response_tokens}")
