b8ae668d08394fa24781a2552d66c71de8bf98c4
IBungy <bcain4902@gmail.com>
2025-08-13 21:46:43 -0400

    added imports

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from google import genai
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
 from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_file_content
+from functions.get_files_info import get_file_conent
+from functions.get_files_info import schema_run_python_file
+from functions.get_files_info import run_python_file
+from functions.get_files_info import schema_write_file
+from functions.get_files_info import write_file
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
