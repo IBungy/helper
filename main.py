1d04494f7148151fd2d5a0badbe3a2f5f3198405
IBungy <bcain4902@gmail.com>
2025-08-13 21:44:46 -0400

    added every functions schemas and added them to available functions and system prompt

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ client = genai.Client(api_key=api_key)
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
     ]
 )
 system_prompt = """
@@ -20,6 +23,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
